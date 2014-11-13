6b8c680d07da2e69f8528cc169db8b752535ddba
Daniel Freitag <daniel.freitag@swisscom.com>
2014-11-13 12:37:35 +0100

    Validation message changed

--- GitTest/src/gittest/City.java
+++ GitTest/src/gittest/City.java
@@ -7,7 +7,7 @@ public class City {
 	
 	public City(String name, Country country) {
 		if (name == null || name.isEmpty()) { 
-		    throw new IllegalArgumentException("Name must not be null or empty"); 
+		    throw new IllegalArgumentException("City name must not be null or empty"); 
 		}
 		if (country == null) {
 			throw new IllegalArgumentException("Country must not be null");
