bba90ddc6ce766b918a5d5e50e2ab7d4f743eb03
Daniel Freitag <daniel.freitag@swisscom.com>
2014-11-13 12:10:08 +0100

    City construction validation added

--- GitTest/src/gittest/City.java
+++ GitTest/src/gittest/City.java
@@ -6,6 +6,12 @@ public class City {
 	private Country country;
 	
 	public City(String name, Country country) {
+		if (name == null || name.isEmpty()) { 
+		    throw new IllegalArgumentException("Name must not be null or empty"); 
+		}
+		if (country == null) {
+			throw new IllegalArgumentException("Country must not be null");
+		}
 		this.name = name;
 		this.country = country;
 	}
